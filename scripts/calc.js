d8594602cd4ad7aae2eba4bcd5df63c7e7e0f9df
FabianMarsh <fabian.marsh00@gmail.com>
2020-07-04 10:15:08 +0000

    whatCanIDrink test created failed

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,11 +1,15 @@
-Calculator = function() {
-    this.value = 0;
-}
-
-Calculator.prototype.add = function(number) {
-    if(typeof(number) == "number"){
-        this.value += number;
-    } else {
-        alert("Error!");
+function whatCanIDrink(){
+    if(number < 0){
+        return("Sorry. I can’t tell what drink because that age is incorrect!");
+    } else if (number < 14){
+        return("Drink Toddy");
+    } else if (number < 18){
+        return("Drink Coke");
+    } else if (number < 21){
+        return("Drink Beer");
+    } else if (number < 130){
+        return("Drink Whisky");
+    } else if (typeOf(number) != Number){
+        return("Sorry. I can’t tell what drink because that age is incorrect!");
     }
 }
