e9145065d6f2195fa8e6a9aac1b8867a1e0ca4cc
FabianMarsh <fabian.marsh00@gmail.com>
2020-07-04 21:52:30 +0000

    fizzBuzz tests

--- scripts/fizzBuzz.js
+++ scripts/fizzBuzz.js
@@ -1,11 +1,9 @@
-fizzBuzz = new function(){
-    for(var i = 0; i < 100; i++){
-        if(i % 3 === 0&& i % 5 === 0){
+fizzBuzz = new function(number){
+        if(number % 3 === 0&& number % 5 === 0){
             return "FizzBuzz";
-        } else if (i % 3 === 0){
+        } else if (number % 3 === 0){
             return "Fizz";
         } else {
             return "Buzz";
         }
-    }
 }
--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,9 +1,3 @@
 describe("My fizzBuzz function", function(){
-    number = new fizzBuzz(), function(){
-        describe("Checks number", function(){
-            it("Should exist", function(){
-                expect(fizzBuzz.toBeDefined());
-            })
-        })  
-    }; 
-});
+    number = new fizzBuzz();
+
